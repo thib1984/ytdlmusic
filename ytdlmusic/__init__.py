709e42ab44cbbc4dfd5447a7214874d98d25ccb7
=thibault.garcon <=thibault.garcon@gmail.com>
2021-04-17 09:14:38 +0200

    clean code

--- ytdlmusic/__init__.py
+++ ytdlmusic/__init__.py
@@ -27,51 +27,44 @@ def ytdlmusic():
         elif len(sys.argv) == 2:
             if sys.argv[1] == "--help":
                 print_help()
-                sys.exit(0)
 
-            if sys.argv[1] == "--update":
+            elif sys.argv[1] == "--update":
                 update()
-                sys.exit(0)
 
-            if sys.argv[1] == "--full-update":
+            elif sys.argv[1] == "--full-update":
                 fullupdate()
-                sys.exit(0)
 
-            if sys.argv[1] == "--version":
+            elif sys.argv[1] == "--version":
                 print_version_ytdlmusic()
                 print_version_dependencies()
                 print_licence()
-                sys.exit(0)
 
-            if sys.argv[1].startswith("--"):
-                print_bad_launch()
-                sys.exit(1)
-
-            print_bad_launch()
-            sys.exit(1)
+            else:
+                newmethod948()
 
         elif len(sys.argv) == 3:
             if sys.argv[1].startswith("--") or sys.argv[2].startswith(
                 "--"
             ):
-                print_bad_launch()
-                sys.exit(1)
+                newmethod948()
             else:
                 job(sys.argv[1], sys.argv[2], False)
-                sys.exit(0)
         elif len(sys.argv) == 4:
             if not sys.argv[1].startswith("--auto"):
                 print_bad_launch()
                 sys.exit(1)
             else:
                 job(sys.argv[2], sys.argv[3], True)
-                sys.exit(0)
         else:
-            print_bad_launch()
-            sys.exit(1)
+            newmethod948()
+        sys.exit(0)    
     except Exception as err:
         print_error(err)
         sys.exit(1)
+
+def newmethod948():
+    print_bad_launch()
+    sys.exit(1)
 
 
 if __name__ == "__main__":
